eb5d27e4cc823fa1e6c68e2b464d14c264ae81dd
hnguyen <hainguyen240195@gmail.com>
2024-09-30 19:26:36 +0300

    I change my code in new branch

--- my_new_file.py
+++ my_new_file.py
@@ -2,4 +2,4 @@ print("This my my new file with a little bit change.")
 
 print("I put in some new code.")
 
-print("here is my new code for push and pull")
+print("here is my new code for conflict merge")
